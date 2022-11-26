40e81a9530431b79fbd27c202806e29e4c5dc1db
e-nikolov <emil.e.nikolov@gmail.com>
2022-11-26 11:04:40 +0100

    fixed some issues

--- selections.plugin.zsh
+++ selections.plugin.zsh
@@ -1,10 +1,6 @@
 # hacky fix for slow Ctrl+X. There are a lot of multi-key combinations starting with Ctrl+X and by default zsh waits for a second before accepting the current command
-export KEYTIMEOUT=1
+# export KEYTIMEOUT=1
 
-export KEY_ALT_F='ƒ'
-export KEY_ALT_B='∫'
-export KEY_ALT_D='∂'
-export KEY_CTRL_U=$'^U' # ^U
 export KEY_CMD_BACKSPACE=$'^[b'   # arbitrary; added via kitty config (send_text)
 export KEY_CMD_Z='^[[122;9u'
 export KEY_SHIFT_CMD_Z=$'^[[122;10u'
@@ -14,6 +10,8 @@ export KEY_CMD_X=$'^[[120;9u'
 export KEY_CMD_V=$'^[[118;9u'
 export KEY_CMD_A=$'^[[97;9u'
 export KEY_CTRL_L=$'^L'
+export KEY_UP="${terminfo[kcuu1]:-$'^[[A'}"
+export KEY_DOWN="${terminfo[kcud1]:-$'^[[B'}"
 export KEY_LEFT="${terminfo[kcub1]:-$'^[[D'}"
 export KEY_RIGHT="${terminfo[kcuf1]:-$'^[[C'}"
 export KEY_SHIFT_UP="${terminfo[kri]:-$'^[[1;2A'}"
@@ -36,6 +34,7 @@ export KEY_SHIFT_CTRL_LEFT=$'^[[1;6D'
 export KEY_SHIFT_CTRL_RIGHT=$'^[[1;6C'
 export KEY_CTRL_D=$'^D'
 export KEY_DELETE=$'^[[3~'
+export KEY_BACKSPACE=$'^?'
 export KEY_CTRL_DELETE=$'^[[3;5~'
 export KEY_END=$'\EOF'
 export KEY_SHIFT_END=$'^[[1;2F'
@@ -47,6 +46,7 @@ export KEY_CTRL_SHIFT_X=$'^]X'
 export KEY_CTRL_SHIFT_V=$'^]V'
 export KEY_CTRL_SHIFT_Z=$'^]Z'
 
+export KEY_CTRL_X=$'^X'
 export KEY_CTRL_V=$'^V'
 export KEY_CTRL_Y=$'^Y'
 export KEY_CTRL_BACKSPACE=$'^H'
@@ -63,9 +63,11 @@ export ACTION_SCROLL_AND_CLEAR_SCREEN=$KEY_CTRL_L
 
 export ACTION_COPY_SELECTION=$KEY_CTRL_SHIFT_C
 export ACTION_CUT_SELECTION=$KEY_CTRL_SHIFT_X
+export ACTION_CUT_SELECTION2=$KEY_CTRL_X
 
 
 export ACTION_FORWARD_DELETE_CHAR=$KEY_DELETE
+export ACTION_BACKWARD_DELETE_CHAR=$KEY_BACKSPACE
 
 export ACTION_FORWARD_KILL_WORD=$KEY_CTRL_DELETE
 export ACTION_BACKWARD_KILL_WORD=$KEY_CTRL_BACKSPACE
@@ -73,6 +75,8 @@ export ACTION_BACKWARD_KILL_LINE=$KEY_CTRL_Y
 
 export ACTION_SELECT_ALL=$KEY_CTRL_A
 
+export ACTION_UNSELECT_UP=$KEY_UP
+export ACTION_UNSELECT_DOWN=$KEY_DOWN
 export ACTION_SELECT_UP=$KEY_SHIFT_UP
 export ACTION_SELECT_DOWN=$KEY_SHIFT_DOWN
 
@@ -90,9 +94,8 @@ export ACTION_SELECT_END_OF_LINE=$KEY_SHIFT_END
 export ACTION_SELECT_BEGINNING_OF_LINE=$KEY_SHIFT_HOME
 export ACTION_UNSELECT_END_OF_LINE=$KEY_END
 export ACTION_UNSELECT_BEGINNING_OF_LINE=$KEY_HOME
-export ACTION_PASTE=$KEY_CTRL_SHIFT_V
-export COPY_COMMAND=xc
-export PASTE_COMMAND=xco
+# export ACTION_PASTE=$KEY_CTRL_SHIFT_V
+export COPY_COMMAND="${COPY_COMMAND:-xclip -sel clip}"
 
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -120,9 +123,7 @@ function widget::select-all() {
     local buflen=$(printf "$BUFFER" | wc -m | bc)
     CURSOR=$buflen   # if this is messing up try: CURSOR=9999999
     zle set-mark-command
-    while [[ $CURSOR > 0 ]]; do
-        zle beginning-of-line
-    done
+    ((CURSOR = 0))
 }
 
 # scrolls the screen up, in effect clearing it
@@ -156,14 +157,14 @@ function widget::util-delselect() {
     fi
 }
 
-# paste clipboard contents
-zle -N widget::paste
-function widget::paste() {
-    xco
-    ((REGION_ACTIVE)) && zle kill-region
-    RBUFFER="$(xco)${RBUFFER}"
-    CURSOR=$(( CURSOR + $(echo -n "$()" | wc -m | bc) ))
-}
+# # paste clipboard contents
+# zle -N widget::paste
+# function widget::paste() {
+#     xco
+#     ((REGION_ACTIVE)) && zle kill-region
+#     RBUFFER="$(xco)${RBUFFER}"
+#     CURSOR=$(( CURSOR + $(echo -n "$()" | wc -m | bc) ))
+# }
 
 function widget::util-insertchar() {
     ((REGION_ACTIVE)) && zle kill-region
@@ -176,69 +177,69 @@ function widget::util-insertchar() {
 
 
 
-for action         seq                   mode        widget (
-    'a'                        'a'               insertchar  'a'
-    'b'                        'b'               insertchar  'b'
-    'c'                        'c'               insertchar  'c'
-    'd'                        'd'               insertchar  'd'
-    'e'                        'e'               insertchar  'e'
-    'f'                        'f'               insertchar  'f'
-    'g'                        'g'               insertchar  'g'
-    'h'                        'h'               insertchar  'h'
-    'i'                        'i'               insertchar  'i'
-    'j'                        'j'               insertchar  'j'
-    'k'                        'k'               insertchar  'k'
-    'l'                        'l'               insertchar  'l'
-    'm'                        'm'               insertchar  'm'
-    'n'                        'n'               insertchar  'n'
-    'o'                        'o'               insertchar  'o'
-    'p'                        'p'               insertchar  'p'
-    'q'                        'q'               insertchar  'q'
-    'r'                        'r'               insertchar  'r'
-    's'                        's'               insertchar  's'
-    't'                        't'               insertchar  't'
-    'u'                        'u'               insertchar  'u'
-    'v'                        'v'               insertchar  'v'
-    'w'                        'w'               insertchar  'w'
-    'x'                        'x'               insertchar  'x'
-    'y'                        'y'               insertchar  'y'
-    'z'                        'z'               insertchar  'z'
-    'A'                        'A'               insertchar  'A'
-    'B'                        'B'               insertchar  'B'
-    'C'                        'C'               insertchar  'C'
-    'D'                        'D'               insertchar  'D'
-    'E'                        'E'               insertchar  'E'
-    'F'                        'F'               insertchar  'F'
-    'G'                        'G'               insertchar  'G'
-    'H'                        'H'               insertchar  'H'
-    'I'                        'I'               insertchar  'I'
-    'J'                        'J'               insertchar  'J'
-    'K'                        'K'               insertchar  'K'
-    'L'                        'L'               insertchar  'L'
-    'M'                        'M'               insertchar  'M'
-    'N'                        'N'               insertchar  'N'
-    'O'                        'O'               insertchar  'O'
-    'P'                        'P'               insertchar  'P'
-    'Q'                        'Q'               insertchar  'Q'
-    'R'                        'R'               insertchar  'R'
-    'S'                        'S'               insertchar  'S'
-    'T'                        'T'               insertchar  'T'
-    'U'                        'U'               insertchar  'U'
-    'V'                        'V'               insertchar  'V'
-    'W'                        'W'               insertchar  'W'
-    'X'                        'X'               insertchar  'X'
-    'Y'                        'Y'               insertchar  'Y'
-    'Z'                        'Z'               insertchar  'Z'
-    '0'                        '0'               insertchar  '0'
-    '1'                        '1'               insertchar  '1'
-    '2'                        '2'               insertchar  '2'
-    '3'                        '3'               insertchar  '3'
-    '4'                        '4'               insertchar  '4'
-    '5'                        '5'               insertchar  '5'
-    '6'                        '6'               insertchar  '6'
-    '7'                        '7'               insertchar  '7'
-    '8'                        '8'               insertchar  '8'
-    '9'                        '9'               insertchar  '9'
+for action         seq               mode               widget (
+    'a'            'a'               insertchar         'a'
+    'b'            'b'               insertchar         'b'
+    'c'            'c'               insertchar         'c'
+    'd'            'd'               insertchar         'd'
+    'e'            'e'               insertchar         'e'
+    'f'            'f'               insertchar         'f'
+    'g'            'g'               insertchar         'g'
+    'h'            'h'               insertchar         'h'
+    'i'            'i'               insertchar         'i'
+    'j'            'j'               insertchar         'j'
+    'k'            'k'               insertchar         'k'
+    'l'            'l'               insertchar         'l'
+    'm'            'm'               insertchar         'm'
+    'n'            'n'               insertchar         'n'
+    'o'            'o'               insertchar         'o'
+    'p'            'p'               insertchar         'p'
+    'q'            'q'               insertchar         'q'
+    'r'            'r'               insertchar         'r'
+    's'            's'               insertchar         's'
+    't'            't'               insertchar         't'
+    'u'            'u'               insertchar         'u'
+    'v'            'v'               insertchar         'v'
+    'w'            'w'               insertchar         'w'
+    'x'            'x'               insertchar         'x'
+    'y'            'y'               insertchar         'y'
+    'z'            'z'               insertchar         'z'
+    'A'            'A'               insertchar         'A'
+    'B'            'B'               insertchar         'B'
+    'C'            'C'               insertchar         'C'
+    'D'            'D'               insertchar         'D'
+    'E'            'E'               insertchar         'E'
+    'F'            'F'               insertchar         'F'
+    'G'            'G'               insertchar         'G'
+    'H'            'H'               insertchar         'H'
+    'I'            'I'               insertchar         'I'
+    'J'            'J'               insertchar         'J'
+    'K'            'K'               insertchar         'K'
+    'L'            'L'               insertchar         'L'
+    'M'            'M'               insertchar         'M'
+    'N'            'N'               insertchar         'N'
+    'O'            'O'               insertchar         'O'
+    'P'            'P'               insertchar         'P'
+    'Q'            'Q'               insertchar         'Q'
+    'R'            'R'               insertchar         'R'
+    'S'            'S'               insertchar         'S'
+    'T'            'T'               insertchar         'T'
+    'U'            'U'               insertchar         'U'
+    'V'            'V'               insertchar         'V'
+    'W'            'W'               insertchar         'W'
+    'X'            'X'               insertchar         'X'
+    'Y'            'Y'               insertchar         'Y'
+    'Z'            'Z'               insertchar         'Z'
+    '0'            '0'               insertchar         '0'
+    '1'            '1'               insertchar         '1'
+    '2'            '2'               insertchar         '2'
+    '3'            '3'               insertchar         '3'
+    '4'            '4'               insertchar         '4'
+    '5'            '5'               insertchar         '5'
+    '6'            '6'               insertchar         '6'
+    '7'            '7'               insertchar         '7'
+    '8'            '8'               insertchar         '8'
+    '9'            '9'               insertchar         '9'
 
     'exclamation-mark'        '!'                insertchar  '!'
     'hash-sign'               '\#'               insertchar  '\#'
@@ -278,6 +279,8 @@ for action         seq                   mode        widget (
     'backward-char'      $ACTION_UNSELECT_BACKWARD_CHAR unselect    backward-char
     'forward-char'       $ACTION_UNSELECT_FORWARD_CHAR  unselect    forward-char
 
+    'up'             $ACTION_UNSELECT_UP     unselect      up-line-or-history
+    'down'           $ACTION_UNSELECT_DOWN   unselect      down-line-or-history
     'up'             $ACTION_SELECT_UP     select      up-line-or-history
     'down'           $ACTION_SELECT_DOWN   select      down-line-or-history
     'forward-char'   $ACTION_SELECT_FORWARD_CHAR      select      forward-char
@@ -293,6 +296,8 @@ for action         seq                   mode        widget (
     'end-of-line'          $ACTION_SELECT_END_OF_LINE           select      end-of-line
     'beginning-of-line'    $ACTION_SELECT_BEGINNING_OF_LINE     select      beginning-of-line
     'delete-char'          $ACTION_FORWARD_DELETE_CHAR          delselect   delete-char
+    'backward-delete-char' $ACTION_BACKWARD_DELETE_CHAR         delselect   backward-delete-char
+    'backward-kill-word'   $ACTION_BACKWARD_KILL_WORD           delselect   backward-kill-word
 ) {
     eval "function widget::action-$mode-$action() {
         widget::util-$mode $widget \$@
@@ -302,12 +307,13 @@ for action         seq                   mode        widget (
 }
 
 bindkey                   $ACTION_FORWARD_KILL_WORD         kill-word
-bindkey                   $ACTION_BACKWARD_KILL_WORD        backward-kill-word
+# bindkey                   $ACTION_BACKWARD_KILL_WORD        backward-kill-word
 bindkey                   $ACTION_BACKWARD_KILL_LINE        backward-kill-line
 bindkey                   $ACTION_UNDO                      undo
 bindkey                   $ACTION_REDO                      redo
 bindkey                   $ACTION_COPY_SELECTION            widget::copy-selection
 bindkey                   $ACTION_CUT_SELECTION             widget::cut-selection
+bindkey                   $ACTION_CUT_SELECTION2            widget::cut-selection
 bindkey                   $ACTION_SELECT_ALL                widget::select-all
 bindkey                   $ACTION_SCROLL_AND_CLEAR_SCREEN   widget::scroll-and-clear-screen
 # bindkey                   $ACTION_PASTE                     widget::paste
